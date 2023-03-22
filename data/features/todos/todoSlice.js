5622ead85c5177f9b99a016763bbef31dda1a7f5
m-sabid <sabidofficial@gmail.com>
2023-03-22 12:51:07 +0600

    add some readme file

--- data/features/todos/todoSlice.js
+++ data/features/todos/todoSlice.js
@@ -1,5 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = createSlice({
-    name:""
-})
+const initialState = {
+  numOfTask: 10,
+};
+
+const todoSlice = createSlice({
+  name: "",
+  initialState,
+  reducers: {
+    task: (state) => {
+      state.numOfTask--;
+    },
+    addTask: (state, action) => {
+      state.numOfTask += action.payload;
+    },
+  },
+});
+
+module.exports = todoSlice.reducer;
+module.exports.todoActions = todoSlice.actions;
