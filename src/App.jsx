191c5d9fa06cbd17d88055785c7a1682d82a7aca
m-sabid <sabidofficial@gmail.com>
2023-03-22 11:11:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="my-5 text-center underline">MY RTK APP</h1>
+
+    </>
+  );
+}
+
+export default App;
